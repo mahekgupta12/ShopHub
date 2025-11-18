7d714e86c4d045cfbf52ce2161dde12f4147b26c
Divya942 <1503chauhandivya@gmail.com>
2025-11-18 22:29:13 +0530

    adding login screen with firebase

---
+++ src/firebase/firebaseConfig.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBT0BETZxtheIggDmUVfQEe83graJHt1IU",
+  authDomain: "shophub-f4dfe.firebaseapp.com",
+  projectId: "shophub-f4dfe",
+  storageBucket: "shophub-f4dfe.firebasestorage.app",
+  messagingSenderId: "1:833568434080:web:25737f033ba21c419926c5",
+  appId: "YOUR_APP_ID",
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
